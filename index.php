52e30eb8b923c5324c474d1b68d81cb1b08a9cf3
marcosOHumilde <marcos.s.brasilleiro@gmail.com>
2017-09-27 01:01:21 -0300

    inicio do projeto

---
+++ index.php
@@ -0,0 +1,59 @@
+<!DOCTYPE html>
+<!--
+To change this license header, choose License Headers in Project Properties.
+To change this template file, choose Tools | Templates
+and open the template in the editor.
+-->
+<html lang="pt-br">
+    <head>
+        <meta charset="UTF-8">
+        <title>Home</title>
+        
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta/css/bootstrap.min.css" integrity="sha384-/Y6pD6FV/Vv2HJnA6t+vslU6fwYXjCFtcEpHbNJ0lyAFsXTsjBbfaDjzALeQsN6M" crossorigin="anonymous">
+        <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta/js/bootstrap.min.js" integrity="sha384-h0AbiXch4ZDo7tp9hKZ4TsHbi047NrKGLO3SEJAg45jXxnGIfYzk4Si90RDIqNm1" crossorigin="anonymous"></script>
+        <link rel="stylesheet" href="../SOSAdvogados/CSS/estilo.css">
+    </head>
+    <body>
+        <div id="form-container"><!--parte de fundo da pagina/-->
+            <div class="panel" id="form-box"><!--container do formulario login-->
+                <form action="login.php" method="post">
+                    <h1 class="text-center">Login</h1>
+                    <div class="form-group"><!--vai conter o label e o input-->
+                        <label class="sr-only" for="login">Cpf</label>
+                        <div class="input-group">
+                            <div class="input-group-addon"><!--divisoria de figura e input-->
+                                <span class="glyphicon glyphicon-user"></span><!--figura-->
+                            </div>
+                            <input type="text" name="inputCpf" class="form-control" placeholder="Cpf"><!--input-->
+                        </div>
+                    </div>  
+                    
+                    <div class="form-group"><!--vai conter o label e o input-->
+                        <label class="sr-only" for="login">Senha</label>
+                        <div class="input-group">
+                            <div class="input-group-addon"><!--divisoria de figura e input-->
+                                <span class="glyphicon glyphicon-lock"></span><!--figura-->
+                            </div>
+                            <input type="password" name="inputSenha" class="form-control" placeholder="Senha"><!--input-->
+                        </div>
+                    </div>  
+                    
+                    <div class="form-group">
+                        <input type="submit" value="Entrar" class="btn btn-success form-control">
+                        </div> 
+                    <div class="form-group">
+                         <a href="conta.php">Criar nova conta!</a>
+                        
+                        </div> 
+                           
+                                        
+                     
+                    
+                </form>            
+            </div>           
+        </div>
+        <?php
+        // put your code here
+        ?>
+    </body>
+</html>
